9e2065c600cc41ef3bbaeb98fac7d3d7f9aba581
Ebraheem <ebraheemsaid7@gmail.com>
2023-05-17 22:58:45 +0300

    adding details and react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Adopt Me!</h1>
+      </div>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
